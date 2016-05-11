63900a6f94162f55e1e9de94021f999cfe1f5386
Cory Tohill <corytohill@gmail.com>
2016-05-11 13:13:58 -0500

    added comments and organized code

--- javascripts/main.js
+++ javascripts/main.js
@@ -44,22 +44,23 @@ $.ajax({url: "songs.json"}).done(manipulateSongInfo);
                         Functions
 *************************************************************/
 
+// given json song data, inserts data into the DOM and adds delete and more buttons
 function manipulateSongInfo (data) {
   $(data.songs).each(function(index, currentSong){
-    let HTMLBuilder =`<div>`
-                    + `<li>${currentSong.title}</li>`
-                    + `<li>${currentSong.artist}</li>`
-                    + `<li>${currentSong.album}</li>`
-                    + `<li>${currentSong.genre}</li>`
-                    + deleteButtonHTML
-                    + `</div>`;
+    let HTMLBuilder = `<div>` +
+                      `<li>${currentSong.title}</li>` +
+                      `<li>${currentSong.artist}</li>` +
+                      `<li>${currentSong.album}</li>` +
+                      `<li>${currentSong.genre}</li>` +
+                      deleteButtonHTML +
+                      `</div>`;
 
     $songElement.append(HTMLBuilder);
-  })
+  });
   $songElement.append(moreButtonHTML);
 }
 
-
+// will add an individual song into DOM using text input for information
 function addSong (){
   $("#moreButton").remove();
 
@@ -67,27 +68,30 @@ function addSong (){
 
   $(".inputInfo").each(function(index, currentInfo){
     songHTML += `<li>${$(currentInfo).val()}</li>`;
-  })
+  });
   $songElement.append(`${songHTML} ${deleteButtonHTML}</div> ${moreButtonHTML}`);
-};
+}
 
-
+// hides the add music div and reveals the list music div
 function showListMusic () {
   addMusic.addClass("hidden");
   listMusic.removeClass("hidden");
 }
 
-
+// hides the list music div and reveals the add music div
 function showAddMusic (){
   listMusic.addClass("hidden");
   addMusic.removeClass("hidden");
 }
 
-
+// determines if target clicked is a "Delete" or "More" button
 function ButtonsBehavior (event) {
+  //Delete button removes the div containing that songs info and the button itself
   if (event.target.className === "delete") {
     event.target.parentNode.remove();
-  } else if (event.target.id === "moreButton") {
+  }
+  // More button loads the data from the moreSongs json file; removes current More button, and inserts data into DOM
+  else if (event.target.id === "moreButton") {
     event.target.remove();
     $.ajax({url: "moreSongs.json"}).done(manipulateSongInfo);
   }
