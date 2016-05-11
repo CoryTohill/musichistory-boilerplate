97544e1069981b0d1d159712c34f72734496cc24
Cory Tohill <corytohill@gmail.com>
2016-05-11 12:10:40 -0500

    converted rest of main.js to jquery

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,127 +1,94 @@
 "use strict";
 
 
+
 /************************************************************
-                      DELETE BUTTONS
+                        Variables
 *************************************************************/
 
-var $songListDiv = $(".songList");
-
-$songListDiv.click(deleteButtonsBehavior);
-
-function deleteButtonsBehavior (event) {
-  if (event.target.className === "delete") {
-    event.target.parentNode.remove();
-  }
-}
-
-
-//MORE BUTTON
-
-$songListDiv.click(moreButtonBehavior);
+var $songElement = $("#songElement"),
+    listMusic = $("#listMusic"),
+    addMusic = $("#addMusic"),
+    deleteButtonHTML = `<button type="button" class="delete">Delete</button>`,
+    moreButtonHTML = `<button type="button" id="moreButton">More</button>`;
 
 
 
 /************************************************************
-          ADD MORE SONGS IN JSON VIA MORE BUTTON
+                      Event Listeners
 *************************************************************/
 
-function moreButtonBehavior (event) {
-  if (event.target.id === "moreButton") {
-    event.target.remove();
-    $.ajax({url: "moreSongs.json"}).done(manipulateSongInfo);
-  }
-}
+// delete and more buttons
+$(".songList").click(ButtonsBehavior);
 
+// add button
+$("#addButton").click(addSong);
+
+// list music link
+$("#listMusicLink").click(showListMusic);
+
+// add music link
+$("#addMusicLink").click(showAddMusic);
 
 
 
 /************************************************************
-                INSERT SONG DATA VIA JSON FILE
+                    Load Initial Json
 *************************************************************/
-
-var songElement = document.getElementById("songElement");
-var deleteButtonHTML = `<button type="button" class="delete">Delete</button>`;
-var moreButtonHTML = `<button type="button" id="moreButton">More</button>`;
 
 $.ajax({url: "songs.json"}).done(manipulateSongInfo);
 
-function manipulateSongInfo (data) {
-  var HTMLBuilder = "";
-  console.log("data", data.songs);
-  var songsData = data;
-
-  for(var i = 0; i < songsData.songs.length; i++) {
-    HTMLBuilder += `<div>`
-               +  `<li>${songsData.songs[i].title}</li>`
-               +  `<li>${songsData.songs[i].artist}</li>`
-               +  `<li>${songsData.songs[i].album}</li>`
-               +  `<li>${songsData.songs[i].genre}</li>`
-               +  deleteButtonHTML
-               +  `</div>`;
-  }
-
-  songElement.innerHTML += HTMLBuilder + moreButtonHTML;
-
-  // moreButtonBehavior();
-
-}
-
-
-
 
 
 /************************************************************
-                      ADD SONGS INPUTS
+                        Functions
 *************************************************************/
 
-var inputInfo = document.getElementsByClassName("inputInfo");
-var addButton = document.getElementById("addButton");
+function manipulateSongInfo (data) {
+  $(data.songs).each(function(index, currentSong){
+    let HTMLBuilder =`<div>`
+                    + `<li>${currentSong.title}</li>`
+                    + `<li>${currentSong.artist}</li>`
+                    + `<li>${currentSong.album}</li>`
+                    + `<li>${currentSong.genre}</li>`
+                    + deleteButtonHTML
+                    + `</div>`;
 
-addButton.addEventListener("click", addSong)
+    $songElement.append(HTMLBuilder);
+  })
+  $songElement.append(moreButtonHTML);
+}
+
 
 function addSong (){
-  moreButton.parentNode.removeChild(moreButton);
-  var holder = `<div>`;
+  $("#moreButton").remove();
 
-  for(var i = 0; i < inputInfo.length; i ++) {
-    holder += `<li>${inputInfo[i].value}</li>`;
-  }
-  holder += `${deleteButtonHTML}</div>`;
-  songElement.innerHTML += holder + moreButtonHTML;
+  let songHTML =`<div>`;
 
-  // moreButtonBehavior();
-  // deleteButtonsBehavior();
-  holder = "";
-}
+  $(".inputInfo").each(function(index, currentInfo){
+    songHTML += `<li>${$(currentInfo).val()}</li>`;
+  })
+  $songElement.append(`${songHTML} ${deleteButtonHTML}</div> ${moreButtonHTML}`);
+};
 
-
-
-/************************************************************
-                    HIDE/SHOW ELEMENTS
-*************************************************************/
-
-var listMusicLink = document.getElementById("listMusicLink");
-var addMusicLink = document.getElementById("addMusicLink");
-var listMusic = document.getElementById("listMusic");
-var addMusic = document.getElementById("addMusic");
-
-
-listMusicLink.addEventListener("click", showListMusic);
-addMusicLink.addEventListener("click", showAddMusic);
 
 function showListMusic () {
-  addMusic.classList.remove("visible");
-  addMusic.classList.add("hidden");
-
-  listMusic.classList.remove("hidden");
-  listMusic.classList.add("visible");
+  addMusic.addClass("hidden");
+  listMusic.removeClass("hidden");
 }
 
-function showAddMusic (){
-  listMusic.classList.remove("visible");
-  listMusic.classList.add("hidden");
 
-  addMusic.classList.remove("hidden");
-  addMusic.classList.add("visible");
+function showAddMusic (){
+  listMusic.addClass("hidden");
+  addMusic.removeClass("hidden");
+}
+
+
+function ButtonsBehavior (event) {
+  if (event.target.className === "delete") {
+    event.target.parentNode.remove();
+  } else if (event.target.id === "moreButton") {
+    event.target.remove();
+    $.ajax({url: "moreSongs.json"}).done(manipulateSongInfo);
+  }
 }
